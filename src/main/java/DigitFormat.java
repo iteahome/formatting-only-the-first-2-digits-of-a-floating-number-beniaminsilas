29260da686a7ccfd42fff1f4ec24c95fb45bbff3
Beni <beniamin.silas@gmail.com>
2020-01-13 19:07:09 +0200

    Formatting using Strings-updated version

--- src/main/java/DigitFormat.java
+++ src/main/java/DigitFormat.java
@@ -2,10 +2,12 @@ import java.text.DecimalFormat;
 
 public class DigitFormat {
     public static void main(String[] args) {
-        DecimalFormat df = new DecimalFormat();
-            df.setMaximumFractionDigits(2);
-        double decimalNumber=1.93211;
-        System.out.format(df.format(decimalNumber));
+       // DecimalFormat df = new DecimalFormat();
+          //  df.setMaximumFractionDigits(2);
+       // double decimalNumber=1.93211;
+       // System.out.format(df.format(decimalNumber));
+        String strDouble = String.format("%.2f", 1.93211);
+        System.out.println(strDouble);
 
     }
 }
