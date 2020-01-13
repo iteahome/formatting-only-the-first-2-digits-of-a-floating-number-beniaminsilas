e6b03d43721535301188affdf468350d93144903
Beni <beniamin.silas@gmail.com>
2020-01-13 19:02:39 +0200

    Full package

--- src/main/java/DigitFormat.java
+++ src/main/java/DigitFormat.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.text.DecimalFormat;
 
 public class DigitFormat {
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat();
+            df.setMaximumFractionDigits(2);
+        double decimalNumber=1.93211;
+        System.out.format(df.format(decimalNumber));
+
+    }
 }
